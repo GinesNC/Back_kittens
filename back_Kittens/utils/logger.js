091b6254df160930da245c1608f6b4b2b986fea0
Ginés Navarrete <ginesnavarretecampos@correo.ugr.es>
2020-09-24 13:34:39 +0200

    Gestion de logs

---
+++ back_Kittens/utils/logger.js
@@ -0,0 +1,30 @@
+var fs = require('fs');
+var moment = require('moment');
+
+var logfile = 'logs/back_kittens.log';
+var logger = function() {};
+
+logger.prototype.log = function(data){
+    data = data.toString();
+
+    fs.appendFile(logfile,
+        moment().format('DD/MM/YYYY HH:mm:ss') + ' --> [Potencial ataque] ' + data +"\n", function (err) {
+        if (err) console.log(err);
+
+        // res.send('');
+    })
+}
+
+logger.prototype.logInsert = function (ip, data){
+    
+    this.log("La IP: [ " + ip + " ] - Intento insertar: [ " + JSON.stringify(data) + " ]");
+   
+}
+
+logger.prototype.logSearch = function (ip, tosearch) {
+
+    this.log("La IP: [ " + ip + " ] - Intento de busqueda con valor: [ " + tosearch + " ]");
+
+}
+
+module.exports = new logger();
