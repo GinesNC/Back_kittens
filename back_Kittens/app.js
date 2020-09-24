c03f9377f26b9442ebced1cb3505707da8029ac1
Ginés Navarrete <ginesnavarretecampos@correo.ugr.es>
2020-09-25 00:53:20 +0200

    Comentado logger dev

--- back_Kittens/app.js
+++ back_Kittens/app.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var ownerRouter = require('./routes/owners');
 
 var companiesROuter = require('./routes/companies');
+var favoriteRouter = require('./routes/favorites');
 
 var app = express();
 
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -19,5 +20,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/owners', ownerRouter);
 app.use('/companies', companiesROuter);
+app.use('/favorites', favoriteRouter);
 
 module.exports = app;
