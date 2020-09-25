c75df6b419667cdb0773252d33fedc45bec600b2
Ginés Navarrete <ginesnavarretecampos@correo.ugr.es>
2020-09-25 11:49:05 +0200

    USado switch para ver cual es la sentencia de la query

--- back_Kittens/utils/dbconnect.js
+++ back_Kittens/utils/dbconnect.js
@@ -23,18 +23,26 @@ const GorestToken = 'f38ed86064790e8126feab64bae5fc1ea43279dc78fd51214a0759367cf
  * @param {callback} callback funcion asincrona para cuando se complete la operación
  */
 function queryexecution(query, data, callback) {
-    var response_json = { code: 200, status: "OK" }
+    var response_json = { code: 200, status: "OK" };
 
     db.query(query, data, function (err, result, field) {
 
         // Si la query es un select se añade el resultado en data
-        if (query.substring(0, 6) === "SELECT") {
-            response_json.data = result;
+        switch (query.substring(0, 6)  ){
+            case "SELECT":
+                response_json.data = result;
+                break;
+            case "UPDATE":
+               if ( result.affectedRows == 0) {
+                    response_json.code = 304;
+                    response_json.status = 'Not Modified';
+               }
+               break;
+             default:
+                
+                break;
         }
-        if (query.substring(0, 6) === "UPDATE" && result.affectedRows == 0) {
-            response_json.code = 304;
-            response_json.status = 'Not Modified';
-        }
+        
 
         if (!err) {
             
